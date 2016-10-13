ae65fc0a4178b6e2c2aa202f319a31d6c8162dd8
Camble <lankythumbs@gmail.com>
2016-10-13 16:06:56 +0100

    Massive refactor
    Untested

--- gbz_power_monitor.py
+++ gbz_power_monitor.py
@@ -1,106 +1,174 @@
 #!/usr/bin/env python2.7
-# date: 30/05/16
-# author: Popcorn <abandonedemails@gmail.com> - Add "Sudomod" in the subject or your message will not be received
+# date: 13/10/16
+# author: Camble
 # version: 1.0a
-# name: GBZ-Power-Monitor - a Power Management utility for the Gameboy Zero project
-# description: a GPIO monitor that detects low battery and power switch status which provides a graceful shutdown facility
-# source: https://github.com/Camble/GBZ-Power-Monitor_PB
+# name: Safe-Power-Monitor - A utility to compliment the Safe Shutdown Switch for the Gameboy Zero project
+# description: A GPIO monitor with graceful shutdown capability and video warning overlays
+# source: 
 
 import RPi.GPIO as GPIO
 import os
 import sys
 import time
 
-batteryGPIO    = 17  # GPIO 17/pin 0
-powerGPIO      = 27  # GPIO 27/pin 2
-sampleRate     = 0.1 # tenth of a second
-batteryTimeout = 10  # 10 seconds
-powerTimeout   = 1   # 1 second
-shutdownVideo  = "~/GBZ-Power-Monitor_PB/lowbattshutdown.mp4" # use no space or non-alphanum characters
-lowalertVideo  = "~/GBZ-Power-Monitor_PB/lowbattalert.mp4"    # use no space or non-alphanum characters
-playerFlag     = 0
+AdafruitPowerBoost  = True # Set this to False if using a generic power booster/charger module ie. a "BangGood" or "GearBest" Module
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(batteryGPIO, GPIO.IN) # No pull_up_down for LBO with voltage clamped with diode
-GPIO.setup(powerGPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+powerGPIO           = 27   # GPIO BCM 27 / Physical Pin 13
+batteryGPIO         = 17   # GPIO BCM 17 / Physical Pin 11
+keepAliveGPIO       = 22   # GPIO BCM 22 / Physical Pin 15
 
-def lowBattery(channel):
-  #Checking for LED bounce for the duration of the battery Timeout
-  for bounceSample in range(1, int(round(batteryTimeout / sampleRate))):
-    time.sleep(sampleRate)
+checksPerSecond     = 4    # How often to check GPIO pins (higher values *may* impact performance and battery life)
+sampleRate          = 0.1  # tenth of a second
+batteryTimeout      = 10   # How long in seconds before acting on low battery
+powerTimeout        = 1    # How long in seconds before acting on power switch
 
-    if GPIO.input(batteryGPIO) is 1:
-       break
-  
-  global playerFlag   
-  while playerFlag is 1:
-    time.sleep(1)
-     
-  #If the LED is a solid condition, there will be no bounce.  Launch shutdown video and then gracefully shutdown
-  if bounceSample is int(round(batteryTimeout / sampleRate)) - 1:
-    playerFlag = 1
-    os.system("/usr/bin/omxplayer --no-osd --layer 999999 " + shutdownVideo + " --alpha 180;sudo shutdown -h now");
-    playerFlag = 0
-    sys.exit(0)
+videoAlpha          = 180  # Alpha transparency for overlaid videos
 
-  #If the LED is a solid for more than 10% of the timeout, we know that the battery is getting low.  Launch the Low Battery alert. 
-  if bounceSample > int(round(batteryTimeout / sampleRate * 0.1)):
-    playerFlag = 1
-    os.system("/usr/bin/omxplayer --no-osd --layer 999999 " + lowalertVideo + " --alpha 160;");
-    playerFlag = 0
-    
-    #Discovered a bug with the Python GPIO library and threaded events.  Need to unbind and rebind after a System Call or the program will crash
-    GPIO.remove_event_detect(batteryGPIO)
-    GPIO.add_event_detect(batteryGPIO, GPIO.BOTH, callback=lowBattery, bouncetime=300)
-    
-    #If we know the battery is low, we aggresively monitor the level to ensure we shutdown once the Power Timeout is exceeded.
-    lowBattery(batteryGPIO)
-    
-def powerSwitch(channel):
-  #Checking for LED bounce for the duration of the Power Timeout
-  for bounceSample in range(1, int(round(powerTimeout / sampleRate))):
-    time.sleep(sampleRate)
+videoPlayer         = "/usr/bin/omxplayer --no-osd --layer 999999"   # Path to video player and switches for overlay layer
+shutdownVideo       = "~/GBZ-Power-Monitor/lowbattshutdown.mp4"      # Use no spaces or non-alphanum characters
+lowalertVideo       = "~/GBZ-Power-Monitor/lowbattalert.mp4"         # Use no spaces or non-alphanum characters
 
-    if GPIO.input(powerGPIO) is 1:
-       break
+# ==================== DO NOT CHANGE ANYTHING BELOW THIS LINE ==================== 
+
+class GpioWatcher():
+  def __init__(self, pin, internal_pull, trigger_state):
+    # Configure GPIO pin
+    self.pin = pin
+    self.pull = internal_pull
+    self.trigger = trigger_state
+
+    # Set edge type for event listener
+    if trigger_state is 0:
+      self.edge = GPIO.FALLING
+    elif trigger_state is 1:
+      self.edge = GPIO.RISING
+
+    # Create a threaded event listener
+    try:
+      GPIO.remove_event_detect(self.pin)
+      GPIO.add_event_detect(self.pin, self.edge, callback=self.Callback, bouncetime=300)
+
+    except KeyboardInterrupt:
+      GPIO.cleanup()
+
+    # If the pin is already triggered, perform the callback
+    if GPIO.input(self.pin) is trigger:
+      Callback()
+
+  def Callback():
+    print "GPIO Pin " + self.pin + " was triggered!"
+
+  pin      = None
+  pull     = None
+  trigger  = None
+  edge     = None
+
+class PowerWatcher(GpioWatcher):
+  def Callback():
+    for bounceSample in range(1, int(round(powerTimeout / sampleRate))):
+      time.sleep(sampleRate)
+
+    if GPIO.input(self.pin) is trigger:
+      break
 
   if bounceSample is int(round(powerTimeout / sampleRate)) - 1:
-      #When the Power Switch is placed in the off position with no bounce for the duration of the Power Timeout, we immediately shutdown
-      os.system("sudo shutdown -h now")
-      try:
-         sys.stdout.close()
-      except:
-         pass
-      try:
-         sys.stderr.close()
-      except:
-         pass
+    # If the power switch is placed in the off position with no bounce, shutdown
+    os.system(self.action)
+    try:
+       sys.stdout.close()
+    except:
+       pass
+    try:
+       sys.stderr.close()
+    except:
+       pass
+    sys.exit(0)
 
-      sys.exit(0)
+class BatteryWatcher(GpioWatcher):
+  def Callback(channel):
+    # Checking for LED bounce for the duration of the battery timeout
+    for bounceSample in range(1, int(round(batteryTimeout / sampleRate))):
+      time.sleep(sampleRate)
+
+      if GPIO.input(self.pin) is not self.trigger:
+         break
+    
+    global playerFlag   
+    while playerFlag is 1:
+      time.sleep(500)
+       
+    # If the LED is a solid condition, there will be no bounce. Launch shutdown video and then gracefully shutdown
+    if bounceSample is int(round(batteryTimeout / sampleRate)) - 1:
+
+      playerFlag = 1
+      os.system(videoPlayer + " " + shutdownVideo + " --alpha 180;")
+      if GPIO.input(self.pin) is not self.trigger
+        break
+      else:
+        os.system("sudo shutdown -h now")
+        playerFlag = 0
+        sys.exit(0)
+
+    # If the LED is a solid for more than 10% of the timeout, launch the low battery alert
+    if bounceSample > int(round(batteryTimeout / sampleRate * 0.1)):
+      playerFlag = 1
+      os.system("/usr/bin/omxplayer --no-osd --layer 999999 " + lowalertVideo + " --alpha 160;")
+      playerFlag = 0
+      
+      # Rebind GPIO event detect after system call (due to a bug with the GPIO library and threaded events)
+      GPIO.remove_event_detect(self.pin)
+      GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=lowBattery, bouncetime=300)
+      
+      # If the battery is low, continue to monitor to ensure safe shutdown after the timeout period
+      self.Callback()
 
 def main():
-  #if the Low Battery LED is active when the program launches, handle it 
-  if GPIO.input(batteryGPIO) is 0:
-    lowBattery(batteryGPIO)
 
-  #if the Power Switch is active when the program launches, handle it
-  if GPIO.input(powerGPIO) is 0:
-    powerSwitch(powerGPIO)
+  # Check /boot/config.txt for dtoverlay line, and add it if required
+  newLine = "dtoverlay=gpio-poweroff,gpiopin=" + keepAliveGPIO + ",active_low=\"y\""
+  file = open("/boot/config,txt", "r")
+  configDone = False
 
-  #Add threaded event listeners for the Low Battery and Power Switch
-  try:
-    GPIO.remove_event_detect(batteryGPIO)
-    GPIO.add_event_detect(batteryGPIO, GPIO.FALLING, callback=lowBattery, bouncetime=300)
+  # Check /boot/config.txt for dtoverlay line
+  while configDone is False:
+    for line in file:
+      if line == newLine:
+        configDone = True
+        file.Close()
+        break
 
-    GPIO.remove_event_detect(powerGPIO)
-    GPIO.add_event_detect(powerGPIO, GPIO.FALLING, callback=powerSwitch, bouncetime=300)
-  except KeyboardInterrupt:
-    GPIO.cleanup()
+      # The line does not exist, add it
+      file = open("/boot/config,txt", "w")
+      file.Write(newLine)
+      configDone = True
+      file.Close()
 
+  # Configure GPIO mode
+  GPIO.setmode(GPIO.BCM)
+
+  # Configure pin settings based on the choice of power supply
+  if AdafruitPowerBoost is True:
+    powerTriggerState       = 0               # 0 = Low, 1 = High
+    batteryTriggerState     = 0               # 0 = Low, 1 = High
+    powerInternalResistor   = GPIO.PUD_DOWN   # Use GPIO.PUD_UP, GPIO.PUD_DOWN, or None
+    batteryInternalResistor = None            # Use GPIO.PUD_UP, GPIO.PUD_DOWN, or None
+
+  elif AdafruitPowerBoost is False:
+    powerTriggerState       = 1               # 0 = Low, 1 = High
+    batteryTriggerState     = 0               # 0 = Low, 1 = High
+    powerInternalResistor   = GPIO.PUD_DOWN   # Use GPIO.PUD_UP, GPIO.PUD_DOWN, or None
+    batteryInternalResistor = None            # Use GPIO.PUD_UP, GPIO.PUD_DOWN, or None
+
+  # Create some GpioWatchers
+  powerWatcher = PowerWatcher(powerGPIO, GPIO.PUD_DOWN, 1)
+  batteryWatcher = BatteryWatcher(batteryGPIO, None, 0)
+
+# Run the program
 main()
 
-#We make an endless loop so the threads running the GPIO events will always be listening, in the future we can add Battery Level monitoring here
+# Wait for GPIO events
+delay = 1000/checksPerSecond
 while True:
-  time.sleep(250)
+  time.sleep(delay)
 
 GPIO.cleanup()
